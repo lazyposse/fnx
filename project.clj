013b4ed123fd52d4da051c8b72d166ef75b56cf7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-03 21:40:10 +0100

    Init the repository.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject my-noir-lab "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/my_noir_lab/test/core.clj
@@ -0,0 +1,6 @@
+(ns my-noir-lab.test.core
+  (:use [my-noir-lab.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
