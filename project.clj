d1efc3c754e278a835a38b51741188dbdf55fd7b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-03 21:44:20 +0100

    Change the project to the good one.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject my-noir-lab "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject my-noir-lab "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.1"]]
+            :main my-noir-lab.server)
+
