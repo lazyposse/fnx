7bed6a7e3626a95af91a8a816647cfd179794d71
denis <denis@vbox-ubuntu-11.04-32b>
2012-01-09 13:53:07 +0100

    meta: added ns-public-fn to list all public fns of a particular ns, will be used in web

--- src/my_noir_lab/meta/example.clj
+++ src/my_noir_lab/meta/example.clj
@@ -1,5 +1,10 @@
-(ns {:doc "Example functions to test the noir-test.meta.expose ns"}
-  noir-test.meta.example)
+(ns ^{:doc "Example functions to test the noir-test.meta.expose ns"}
+  my-noir-lab.meta.example)
 
-(defn hello-noir
+(defn hello-noir "A public fn that should be listed"
   [] (str "Hello noir, today we are " (java.util.Date.)))
+
+(defn- hello-private "A private fn that shouldn't be listed"
+  [] (str "Hello noir, today we are " (java.util.Date.)))
+
+(def a-public-var-that-shouldnt-be-list 0)
--- src/my_noir_lab/meta/expose.clj
+++ src/my_noir_lab/meta/expose.clj
@@ -1,16 +1,21 @@
 (ns ^{:doc "Allow to list functions in namespaces, usefull for exposing funs in a web app for example."}
-  noir-test.meta.expose
-  (:use [clojure.repl :only [doc]]))
+  my-noir-lab.meta.expose
+  (:use [clojure.repl :only [doc]])
+  (:use [midje.sweet])
+  ;; TODO : we will need to not include it (should be dynamic)
+  (comment (:require [my-noir-lab.meta.example :as e])))
 
-(defn ns-public-fn
-  "Given a ns symbol, returns all the public fns of this ns"
-  [ns] (filter #(:arglists %)
-               (vals (ns-publics (find-ns ns)))))
+(println "--------- BEGIN OF EXPOSE  ----------" (java.util.Date.))
 
-(defn ns-public-fn
-  "Given a ns symbol, returns all the public fns of this ns"
-  [ns] 
-  (vals (ns-publics (find-ns ns))))
+(defn ns-public-fn "Given a ns symbol, returns all the public fns of this ns"
+  [ns] (do (require ns)
+           (filter #(:arglists (meta %))
+                   (vals (ns-publics (find-ns ns))))))
 
-(comment
-  ((var-get ('hello-noir (ns-publics *ns*)))))
+(fact "ns-public-fn"
+      (map str (ns-public-fn 'my-noir-lab.meta.example)) => ["#'my-noir-lab.meta.example/hello-noir"])
+
+(fact "ns-public-fn: fn has args meta data"
+      (:arglists (meta (first (ns-public-fn 'my-noir-lab.meta.example)))) => ["#'my-noir-lab.meta.example/hello-noir"])
+
+(println "--------- END OF EXPOSE  ----------" (java.util.Date.))
