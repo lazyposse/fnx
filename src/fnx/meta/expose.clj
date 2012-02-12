c9704e593bb183b02fcb18acc10f03a44c5630bd
Denis Labaye <denis.labaye@gmail.com>
2012-02-12 11:30:35 +0100

    added marginalia dep + added marginalia compatible doc

--- src/fnx/meta/expose.clj
+++ src/fnx/meta/expose.clj
@@ -1,15 +1,17 @@
-(ns ^{:doc "Allow to list functions in namespaces, usefull for exposing funs in a web app for example."}
-  fnx.meta.expose
-  (:use [clojure.repl :only [doc]])
+(ns fnx.meta.expose
+  "Get the public functions of a namespace"
   (:use [midje.sweet]))
 
-(println "--------- BEGIN OF EXPOSE  ----------" (java.util.Date.))
 
-(defn ns-public-fn "Given a ns symbol, returns all the public fns of this ns"
-  [ns](do (require ns)
-          (filter (fn [s] (:arglists (meta s)))
-                  (map #(ns-resolve ns %)
-                       (keys (ns-publics (find-ns ns)))))))
+;; * First we load the ns with `require`
+;; * Then we use `ns-publics` to get all public vars
+;; * Then we filter functions: They have an `:arglists` in their meta
+(defn ns-public-fn
+  "Given a ns symbol, returns all the public fns of this ns."
+  [ns] (do (require ns)
+           (filter (fn [s] (:arglists (meta s)))
+                   (map #(ns-resolve ns %)
+                        (keys (ns-publics (find-ns ns)))))))
 
 (fact "ns-public-fn"
       (second (ns-public-fn 'fnx.meta.example)) => (resolve 'fnx.meta.example/hello-noir))
@@ -18,5 +20,3 @@
       (map (fn [f] (apply f
                          (range 0 (count (first (:arglists (meta f)))))))
            (ns-public-fn 'fnx.meta.example)) => ["arg=0", "Hello noir" "args=0,1"])
-
-(println "--------- END OF EXPOSE  ----------" (java.util.Date.))
