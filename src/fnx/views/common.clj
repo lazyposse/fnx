e9594724c7385b801f5e78f0839e1a02d808e26f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-02-19 20:50:55 +0100

    Change the URI exposed - issue https://github.com/clojure-consulting/fnx/issues/22.

--- src/fnx/views/common.clj
+++ src/fnx/views/common.clj
@@ -2,15 +2,42 @@
   "Common functions to help in displaying ui."
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-css html5]]
-        [hiccup.form-helpers :only [label text-field]]))
+        [hiccup.form-helpers :only [form-to hidden-field submit-button label text-field]]
+        [fnx.meta.expose :only [ns-public-fn list-ns dot2uri fun-str]]))
 
 ;; The default site layout
 (defpartial layout [t & content]
   (html5
-    [:head
-     [:title t]
+   [:head
+    [:title t]
     (include-css "/css/reset.css")]
-    [:body
-      [:div#wrapper
-       content]]))
+   [:body
+    [:div#wrapper
+     content]]))
 
+(defn input-fields-ns "Wrapper to expose a namespace."
+  [ns]
+  (let [m (meta ns)]
+    (form-to [:get (str "/" (dot2uri (str ns)) "/")]
+             (submit-button "+")
+             [:b (label "ns" ns)]
+             (label "Doc :" [:i (:doc m)]))))
+
+(defn input-fields-fn "Wrapper to expose a function."
+  [fun]
+  (let [m (meta fun)]
+    (form-to [:get (str "/" (dot2uri (fun-str (str fun))))]
+             (label "fun" fun)
+             (label "Doc :" [:i (:doc m)] )
+             (map #(text-field % "") (flatten (:arglists m)))
+             (submit-button "run"))))
+
+;; Display the list of namespaces
+(defpartial deal-with-ns [{:keys [ns namespaces title]}]
+  (layout
+   title
+   [:h1 title]
+   [:p#wrapper
+    (if namespaces
+      (map input-fields-ns namespaces)
+      (str "No namespace matching " ns))]))
--- src/fnx/views/welcome.clj
+++ src/fnx/views/welcome.clj
@@ -3,67 +3,51 @@
   (:use [noir.core :only [defpage render]]
         [hiccup.core :only [html]]
         [hiccup.form-helpers :only [text-field label form-to submit-button hidden-field]]
-        [fnx.meta.expose :only [ns-public-fn list-ns]]
+        [fnx.meta.expose :only [ns-public-fn list-ns fn-name resolve-str]]
         [fnx.views.common]))
 
-;; A page to execute function
-(defpage [:post "/xfn"] {:keys [fun] :as data}
-  (let [g (fnx.meta.expose/resolve-str fun)
-        k (dissoc data :fun)]
-    (layout
-     "run"
-     [:h1 (str "run " g " vals " k)]
-     [:p#wrapper (apply g (vals k))])))
+;; A page to expose the namespaces of the application
+;; GET /fnx will display the 'fnx' namespaces
+(defpage "/:ns/" {ns :ns}
+  (let [m {:ns ns
+           :namespaces (list-ns ns)
+           :title (str "Exposed namespaces - '" ns "'")}]
+    (deal-with-ns m)))
 
-(defn input-fields-fn "Wrapper to expose a function."
-  [fun]
-  (let [m (meta fun)]
-    (form-to [:post "/xfn"]
-             (label "fun" fun)
-             (hidden-field "fun" fun)
-             (label "Doc :" (str "<i>" (:doc m) "</i>"))
-             (map #(text-field % "") (flatten (:arglists m)))
-             (submit-button "run"))))
+;; If no special route is given, will reroute to /fnx
+(defpage "/" []
+  (render "/:ns/" {:ns "fnx"}))
 
-(defn input-fields-ns "Wrapper to expose a namespace."
-  [ns]
-  (let [m (meta ns)]
-    (form-to [:post "/expose"]
-             (submit-button "+")
-             (hidden-field "ns" ns)
-             (label "ns" ns)
-             (label "Doc :" (str "<i>" (:doc m) "</i>")))))
+;; A page to expose the namespaces of the application
+;; GET /fnx/meta will display the 'fnx.meta' namespaces
+(defpage "/:ns0/:ns1/" {:keys [ns0 ns1]}
+  (let [ns (str ns0 "." ns1)
+        m {:ns ns
+           :namespaces (list-ns ns)
+           :title (str "Exposed namespaces - '" ns "'")}]
+    (deal-with-ns m)))
 
 ;; A page to expose some functions from a namespace.
-;; fnx.meta.example is the default one is no other is supplied.
-(defpage [:post "/expose"] {:keys [ns]}
-  (let [n (symbol ns)
+(defpage "/:ns0/:ns1/:ns2/" {:keys [ns0 ns1 ns2]}
+  (let [n (symbol (clojure.string/join "." [ns0 ns1 ns2]))
         dn (:doc (meta (find-ns n)))
         title n]
     (layout
      title
      [:h1 title]
      [:p#wrapper
-      (str "<i>" dn "</i>") 
+      [:i dn]
       (let [f (ns-public-fn n)]
         (if f
           (map input-fields-fn f)
-          (str "Huh? No function exposed in " n)))])))
+          (str "WARNING - Empty namespace!? " n)))])))
 
-;; A page to expose the namespace of the applications
-;; http://localhost/fnx will display all the namespaces of fnx
-(defpage "/:ns" {ns :ns}
-  (let [namespaces (list-ns ns)
-        title (str "Exposed namespaces - '" ns "'")]
+;; A page to execute a function
+;; GET /fnx/meta/expose/test will execute the function fnx.meta.expose/test
+(defpage"/:ns0/:ns1/:ns2/:fun" {:keys [ns0 ns1 ns2 fun] :as data}
+  (let [g (resolve-str (fn-name ns0 ns1 ns2 fun))
+        k (dissoc data :ns0 :ns1 :ns2 :fun)]
     (layout
-     title
-     [:h1 title]
-     [:p#wrapper
-      (if namespaces
-        (map input-fields-ns namespaces)
-        (str "No namespace with " ns))])))
-
-;; Routing by default on fnx
-(defpage "/" []
-  (render "/:ns" {:ns "fnx"}))
-
+     "run"
+     [:h1 (str "run " g " vals " k)]
+     [:p#wrapper (apply g (vals k))])))
